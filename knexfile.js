87c60ab10f41645d8f88863589e0f7d80beb16cf
Jordan Reed <jordanr23@gmail.com>
2017-10-16 13:03:07 -0600

    heroku enabled

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/cartoons'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   },
 
 };
